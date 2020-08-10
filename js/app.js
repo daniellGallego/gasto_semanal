00c4b60efce6de20e370129bd89278a4c60aa809
DanielGC <dagenelmic@gmail.com>
2020-08-10 10:23:00 -0500

    insertar valores a la interfaz presupuesto y restante

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
-// variables 
+// variables
 
-const presupuestoUsuario = prompt('¿cual es tu presupuesto semanal?');
+const presupuestoUsuario = prompt("¿cual es tu presupuesto semanal?");
 let cantidadPresupuesto;
 
-
-// clases 
+// clases
 // clase de presupuesto
 class Presupuesto {
     constructor(presupuesto) {
@@ -13,23 +12,35 @@ class Presupuesto {
     }
     // metodo para ir restando del presupuesto actual
     presupuestoRestante(cantidad = 0) {
-        return this.restante == Number(cantidad);
+        return (this.restante -= Number(cantidad));
+    }
+}
+
+// clase de interfaz todo lo relacionado con el presupuesto
+
+class Interfaz {
+    insertarPresupuesto(cantidad) {
+        const presupuestoSpan = document.querySelector('span#total');
+        const restanteSpan = document.querySelector('span#restante');
+
+        presupuestoSpan.innerHTML = `${cantidad}`;
+        restanteSpan.innerHTML = `${cantidad}`;
 
     }
 
 }
 
-
-
 // event listeners
-document.addEventListener('DOMContentLoaded', function () {
-
+document.addEventListener("DOMContentLoaded", function () {
     if (presupuestoUsuario === null || presupuestoUsuario === "") {
         window.location.reload();
     } else {
         // instanciar el presupuesto
         cantidadPresupuesto = new Presupuesto(presupuestoUsuario);
 
-        console.log(cantidadPresupuesto)
+        console.log(cantidadPresupuesto);
+        // insertar el presupuesto a la interfaz
+        const ui = new Interfaz();
+        ui.insertarPresupuesto(cantidadPresupuesto.presupuesto);
     }
 });
